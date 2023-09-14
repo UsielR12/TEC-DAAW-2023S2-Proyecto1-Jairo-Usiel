fe8c588bdb1b2ac339f97958d589a7a453b7c86e
usiel12 <usiel.ramirez@concasa.com>
2023-09-14 01:44:20 -0600

    Agregar imagenes, arreglar estilos
    agregué las ultimas imagenes faltantes y arregle unos detalles esteticos

--- controllers/main.js
+++ controllers/main.js
@@ -4,6 +4,7 @@ const startButton = document.getElementById('startButton')
 startButton.addEventListener('click', function() {
     const textboxContent = nicknameTextbox.value
     localStorage.setItem('nickname', textboxContent)
-    localStorage.setItem('points', 0)
+    localStorage.setItem('points', 100)
     window.location.href = '/firstStage'
 })
+
